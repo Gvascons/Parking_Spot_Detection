46b7367969c6cbdaa7d869d989bd37d7c1d39d55
João Gabriel de Araújo Vasconcelos <39220533+Gvascons@users.noreply.github.com>
2022-10-19 18:00:43 -0600

    Add files via upload

---
+++ utils/rename_files.py
@@ -0,0 +1,24 @@
+import os
+
+# Insert the folder to rename the files
+folder = r'#'
+# Where does the count start?
+count = 0
+# count increase by 1 in each iteration
+# iterate all files from a directory
+for file_name in os.listdir(folder):
+    # Construct old file name
+    source = folder + file_name
+
+    # Adding the count to the new file name and extension
+    destination = folder + "frame" + str(count) + ".txt"
+
+    # Renaming the file
+    os.rename(source, destination)
+    count += 1
+print('All Files Renamed')
+
+print('New Names are')
+# verify the result
+res = os.listdir(folder)
+print(res)
